d363aec0d31383a8f9a864780c8c27870c40749f
Kolja Lampe <razzeee@gmail.com>
2025-01-30 21:00:17 +0100

    Add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://razzeee.github.io",
   integrations: [
     react(),
     tailwind({
